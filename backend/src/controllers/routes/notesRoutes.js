561b5793c473fe58df45a6ef67cd557aa0097a73
Akshay-K-M <akshaykmfilms@gmail.com>
2025-09-22 19:30:03 +0530

    Backend setup

---
+++ backend/src/controllers/routes/notesRoutes.js
@@ -0,0 +1,13 @@
+import express from "express"
+import { getAllNotes, createNote, deleteNote, updateNote } from "../src/controllers/notesController.js";
+const router=express.Router();
+
+router.get("/sth",getAllNotes);
+
+router.post("/",createNote);
+
+router.put("/:id",updateNote);
+
+router.delete("/:id",deleteNote);
+
+export default router
