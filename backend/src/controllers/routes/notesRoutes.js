87b185cb40e21cfc1555de0289fd7599858c30e6
Akshay-K-M <akshaykmfilms@gmail.com>
2025-09-23 22:48:38 +0530

    minor changes

--- backend/src/controllers/routes/notesRoutes.js
+++ backend/src/controllers/routes/notesRoutes.js
@@ -2,7 +2,7 @@ import express from "express"
 import { getAllNotes, createNote, deleteNote, updateNote } from "../src/controllers/notesController.js";
 const router=express.Router();
 
-router.get("/abc",getAllNotes);
+router.get("/",getAllNotes);
 
 router.post("/",createNote);
 
