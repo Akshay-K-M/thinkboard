61328cc171a6e7088f2fbee0d920005e0521239b
Akshay-K-M <akshaykmfilms@gmail.com>
2025-10-02 18:19:54 +0530

    Ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,24 @@
 import express from "express";
 import dotenv from "dotenv"
 import cors from "cors";
+import path from "path";
+
 import rateLimiter from "./middleware/rateLimiter.js";
 import notesRoutes from "./routes/notesRoutes.js";
 import {connectDB} from "./config/db.js";
+
 dotenv.config();
 
 const app=express()
 const PORT=process.env.PORT || 5001
+const __dirname=path.resolve();
 
 // middleware
-app.use(cors({
-    origin: "http://localhost:5174",
-}));
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors({
+        origin: "http://localhost:5174",
+    }));
+}
 app.use(express.json());
 app.use(rateLimiter);
 // fun middleware made for learning
@@ -21,6 +27,14 @@ app.use(rateLimiter);
 //     next();
 // })
 app.use("/api/notes", notesRoutes);
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
